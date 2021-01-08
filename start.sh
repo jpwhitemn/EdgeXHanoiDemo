5f4b2e8b03d1db93573bc29f825b039476d34444
jpwhitemn <jpwhite_mn@yahoo.com>
2021-01-08 13:35:27 -0700

    changed directory of app service
    Signed-off-by: jpwhitemn <jpwhite_mn@yahoo.com>

--- start.sh
+++ start.sh
@@ -11,7 +11,7 @@ cd /home/jim/forks/device-snmp-go/cmd
 
 ## Go to App Service Influx and start it
 echo "Starting App Service Influx"
-cd /home/jim/forks/app-service-influx/cmd
+cd /home/jim/forks/app-service-influx
 ./app-service-influx >/dev/null 2>&1 &
 
 echo "Device Services and App Service all started"
